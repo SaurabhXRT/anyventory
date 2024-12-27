162e048bc9594b83ad6edca9fa30792a392c646d
Saurabh Kumar <saurabhbabu287@gmail.com>
2024-12-27 21:37:18 +0530

    initialised with tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import "./global.css"
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex-1 justify-center items-center bg-black'>
+      <Text className='text-white'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
